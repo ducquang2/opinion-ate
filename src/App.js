85b756e9069a57d8a25577178b202919f421a2e1
ducquang2 <quangbilun@gmail.com>
2021-04-13 22:35:03 +0700

    step 3 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import {Provider} from 'react-redux';
+import store from './store';
 import RestaurantScreen from './components/RestaurantScreen';
 
 const App = () => (
-  <div>
+  <Provider store={store}>
     Hello, world.
     <RestaurantScreen />
-  </div>
+  </Provider>
 );
 
 export default App;
